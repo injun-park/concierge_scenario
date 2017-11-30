38841de1b53ed68436eb0f599e2fc0f8ddbf21ea
ijpark <injun.park.p@gmail.com>
2017-11-30 16:17:18 +0900

    UI 수정

--- scripts/dialogue.py
+++ scripts/dialogue.py
@@ -12,33 +12,28 @@ from user_detector import CannotDetectUserException
 from ui_control import UI_Control
 
 
-class Composer :
+class RequestHandler :
     def __init__(self): pass
+
+class Dialogue :
+    def __init__(self):
+        pass
 
     def initialize(self):
         self.tts = TTS()
         self.ui = UI_Control()
         self.conversation = Conversation()
-        self.user_detector = UserDetector()
-
-        self.user_detector.startDetection()
-        self.user_detector.checkDetection()
-
         sentence = "사용자가 감지 되었습니다. 저와 이야기 하고 싶으면 안녕이라고 말씀해주세요. "
         self.ui.sendRobotMsg(sentence)
         self.tts.speak(sentence)
 
     def main(self):
         is_shutdown = False
-        while is_shutdown is not True :
+        while is_shutdown is not True:
             self.initialize()
             conversation_finish = False
             try:
-                while conversation_finish is not True :
-
-                    if not self.user_detector.isDetected() :
-                        raise CannotDetectUserException()
-
+                while conversation_finish is not True:
                     conversation_result = self.get_converation_result()
                     response_sentences = conversation_result['output']['text'][0]
                     self.ui.sendRobotMsg(response_sentences)
@@ -50,36 +45,34 @@ class Composer :
                     except KeyError as e:
                         pass
 
-                    try :
+                    try:
                         entities = conversation_result['entities']
-                        for entity in entities :
-                            if entity['entity'] == "shutdown" :
+                        for entity in entities:
+                            if entity['entity'] == "shutdown":
                                 conversation_finish = True
                                 is_shutdown = True
-                    except KeyError as e :
+                    except KeyError as e:
                         pass
 
-            except CannotDetectUserException as e :
+            except CannotDetectUserException as e:
                 self.tts.speak("사용자가 사라졌습니다. 초기 상태로 돌아 갑니다.")
 
         rospy.loginfo("main loop finish")
 
-
     def get_converation_result(self):
-        def mic_callback() :
+        def mic_callback():
             self.ui.sendUserInputWating()
 
         #
         # speech callback function
         #
-        def speech_callback(speech_result) :
+        def speech_callback(speech_result):
             print('Confidence: {}'.format(speech_result.confidence))
             print('Transcript: {}'.format(speech_result.transcript))
             print "is_final : ", speech_result.isFinal
             print ""
 
             self.ui.sendUserMsg(speech_result.transcript)
-
 
         speech_result = stt_google.recognize(speech_callback, mic_callback)
         aibril_response = self.conversation.getResponse(speech_result.transcript)
@@ -88,9 +81,9 @@ class Composer :
 
         return aibril_response
 
-if __name__ == "__main__" :
-    rospy.init_node('scenario_composer', anonymous=True)
-    composer = Composer()
-    composer.main()
-    rospy.spin()
 
+if __name__ == "__main__":
+    rospy.init_node('scenario_composer', anonymous=True)
+    dialogue = Dialogue()
+    dialogue.main()
+    rospy.spin()
